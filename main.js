e902ffdbffb82a07832aecbba236dd8d0079dd25
Pavlov <a.pavlov@alfatell.ru>
2019-10-31 16:58:21 +0400

    Replace icon and add logic to build table

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    icon: './cpu.png',
+    width: 1024,
+    height: 768,
+    icon: `${__dirname}/assets/app.ico`,
     webPreferences: {
       nodeIntegration: true
     }
--- src/components/Scheduler.vue
+++ src/components/Scheduler.vue
@@ -1,23 +1,86 @@
 <template>
   <div>
-    {{msg}}
+    <div style="text-align: center">{{msg}}</div>
 
-    <b-table striped bordered :items="scheduleData"></b-table>
+    <b-form-input type="number" min="2019" max="2100" v-model="inputData.year"/>
+    <b-form-input type="number" min="1" max="12" v-model="inputData.month"/>
+    <b-form-textarea v-model="inputData.persons" placeholder="Иванов, Петров, Сидоров" rows="1" max-rows="6"></b-form-textarea>
+
+    <b-button v-on:click="initTable">Сформировать</b-button>
+    <b-table striped bordered small :items="scheduleData.data" :fields="scheduleData.fields"></b-table>
   </div>
 </template>
 <script>
   export default {
-    name: 'Scheduler',
-    data () {
-      return {
-          msg: 'Scheduler AA',
-          scheduleData: [
-              { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-              { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-              { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-              { age: 38, first_name: 'Jami', last_name: 'Carney' }
-          ]
+      name: 'Scheduler',
+      data () {
+        return {
+            msg: 'График учета дежурств',
+            scheduleData: {
+                data: [],
+                fields: [],
+                countDaysOfDate: 0
+            },
+            inputData: {
+                year: new Date().getFullYear(),
+                month: new Date().getMonth() + 1,
+                persons: 'Петров, Иванов, Валентинов'
+            }
+        }
+      },
+      methods: {
+
+          /**
+           * Initialize schedule table
+           */
+          initTable() {
+              let persons = this.inputData.persons.replace(/\s+/g, '').split(',');
+              let me = this;
+              this.scheduleData = this.getData(persons, this.inputData.year, this.inputData.month);
+
+              let am = this.scheduleData.countDaysOfDate / persons.length;
+              console.log('am', am)
+              persons.forEach(v => {
+                  for (let i = 1; i < me.scheduleData.countDaysOfDate + 1; i++) {
+                      console.log('n',me.scheduleData.data)
+                      console.log('v',me.scheduleData.data)
+                      if (me.scheduleData.data.name === v) {
+                          me.scheduleData.data[(i+persons.length).toString()] = 'X'
+                      }
+
+                  }
+              })
+          },
+
+          /**
+           * Get data to initialize schedule table
+           * where data - information to build schedule table, fields - to rename rows
+           * @param names - Array[String] - names of persons
+           * @param year  - searchable year
+           * @param month - searchable month
+           * @return {{data: [], fields: [], countDaysOfDate: number}}
+           */
+          getData(names, year, month) {
+              let count = new Date(year, month, 0).getDate();
+              let response = { data: [], fields: [], countDaysOfDate: count };
+              let fieldsValue = {};
+              fieldsValue['key'] = 'name';
+              fieldsValue['label'] = 'Фамилия';
+              response.fields.push(fieldsValue);
+              names.forEach(person => {
+                  let dataValue = {};
+                  dataValue['name'] = person;
+                  for (let i = 1; i <= count; i++) {
+                      dataValue[i.toString()] = '';
+                      let fieldsValue = {}
+                      fieldsValue['key'] = i.toString();
+                      fieldsValue['label'] = i.toString();
+                      response.fields.push(fieldsValue);
+                  }
+                  response.data.push(dataValue);
+              });
+              return response;
+          }
       }
-    }
   }
 </script>
